a7b31b4d3bb627e232f285190306174f3c6f718c
toolsdevaccount <sakurai79@hotmail.co.jp>
2023-05-11 17:13:50 +0900

    注文書、染色依頼書開発途中コミット

--- viewspopdf.py
+++ viewspopdf.py
@@ -1,9 +1,8 @@
 from django.http import HttpResponse
-from django.shortcuts import render,redirect
 from reportlab.pdfgen import canvas
 from reportlab.pdfbase import pdfmetrics
 from reportlab.pdfbase.cidfonts import UnicodeCIDFont
-from reportlab.lib.pagesizes import A4, landscape
+from reportlab.lib.pagesizes import A4, portrait, landscape
 from reportlab.platypus import Table, TableStyle
 from reportlab.lib.units import mm
 from reportlab.lib import colors
@@ -15,12 +14,11 @@ import datetime
 
 from reportlab.platypus import Paragraph
 from reportlab.lib.styles import ParagraphStyle, ParagraphStyle
-from reportlab.lib.styles import getSampleStyleSheet
 from reportlab.lib.enums import TA_JUSTIFY, TA_RIGHT, TA_CENTER, TA_LEFT
  
 def pdf(request,pk):
     strtime = timezone.now() + datetime.timedelta(hours=9)
-    filename = "estimate_" + strtime.strftime('%Y%m%d%H%M%S')
+    filename = "PurchaseOrder_" + strtime.strftime('%Y%m%d%H%M%S')
     make(pk,filename)
     response = HttpResponse(open('./download/' + filename + '.pdf','rb').read(), content_type='application/pdf')
     response['Content-Disposition'] = 'attachment; filename=' + filename + '.pdf'
@@ -28,9 +26,13 @@ def pdf(request,pk):
     return response
 
 def make(pk,filename):
-    pdf_canvas = set_info(filename) # キャンバス名
     dt = connect(pk)
-    print_string(pdf_canvas,dt)
+    if dt[0][43]==1:
+        pdf_canvas = set_info(filename) # キャンバス名
+        print_string(pdf_canvas,dt)
+    if dt[0][43]==2 or dt[0][43]==3:
+        pdf_canvas = set_info_stain(filename) # キャンバス名
+        print_string_StainRequest(pdf_canvas,dt)
     pdf_canvas.save() # 保存
 
 def set_info(filename):
@@ -38,6 +40,13 @@ def set_info(filename):
     pdf_canvas.setAuthor("hpscript")
     pdf_canvas.setTitle("注文書")
     pdf_canvas.setSubject("注文書")
+    return pdf_canvas
+
+def set_info_stain(filename):
+    pdf_canvas = canvas.Canvas("./download/{0}.pdf".format(filename),pagesize=portrait(A4))
+    pdf_canvas.setAuthor("hpscript")
+    pdf_canvas.setTitle("染色依頼注文書")
+    pdf_canvas.setSubject("染色依頼注文書")
     return pdf_canvas
 
 def connect(pk):
@@ -49,7 +58,7 @@ def connect(pk):
                 ' ,c.BuildingName,b.DetailItemNumber,b.DetailColorNumber,b.DetailColor,b.DetailTailoring,FORMAT(b.DetailVolume,2),FORMAT(b.detailunitprice,0)'
                 ' ,b.detailsummary,DATE_FORMAT(b.AnswerDeadline,"%Y年%m月%d日"),e.CustomerName,e.PostCode,g.prefecturename,e.Municipalities,e.Address'
                 ' ,e.BuildingName,e.PhoneNumber,e.FaxNumber,d.first_name,d.last_name,d.email,f.CustomerName,f.PostCode,i.prefecturename,f.Municipalities'
-                ' ,f.Address,f.BuildingName,f.PhoneNumber,f.FaxNumber'
+                ' ,f.Address,f.BuildingName,f.PhoneNumber,f.FaxNumber,a.StainMixRatio,a.OutputDiv'
            ' FROM '
                 'myapp_orderingtable a '
            ' LEFT JOIN myapp_orderingdetail b on a.id = b.OrderingTableId_id'
@@ -61,7 +70,8 @@ def connect(pk):
            ' LEFT JOIN title j on a.titlediv = j.titledivcode'
            ' ,(SELECT PostCode,CustomerName,PrefecturesCode,Municipalities,Address,BuildingName,PhoneNumber,FaxNumber FROM myapp_customersupplier WHERE CustomerCode = "A00042") e'
 	       ' LEFT JOIN prefecture g on e.PrefecturesCode = g.prefecturecode'
-           ' WHERE a.id = ' + str(pk) + ' AND a.OutputDiv=1 AND b.is_Deleted= 0' 
+           ' WHERE a.id = ' + str(pk) + ' AND b.is_Deleted= 0' 
+           ' ORDER BY b.DetailItemNumber ASC' 
         )
     try:
         cur.execute(sql)
@@ -200,6 +210,185 @@ def print_string(pdf_canvas,dt):
     pdf_canvas.drawString(50, 60, '　出荷次第、納品書を翌日当社宛に発注番号を必ずご記入の上、ご一報ください。')
 
     pdf_canvas.rect(43, 20, 765, 100) 
+ 
+    pdf_canvas.showPage()
+     
+if __name__ == '__main__':
+    make()
+
+def print_string_StainRequest(pdf_canvas,dt):
+    # フォント登録
+    pdfmetrics.registerFont(UnicodeCIDFont('HeiseiKakuGo-W5'))
+
+    width, height = A4
+
+    # title
+    font_size = 20
+    pdf_canvas.setFont('HeiseiKakuGo-W5', font_size)
+    pdf_canvas.drawString(240, 800, '染 付 依 頼 書')
+
+    # 発注番号
+    font_size = 9
+    pdf_canvas.setFont('HeiseiKakuGo-W5', font_size)
+    pdf_canvas.drawString(450, 790, '発注番号: ' + dt[0][0] + dt[0][1])
+    #pdf_canvas.line(680, 365, 808, 365) 
+
+    # 依頼日
+    font_size = 9
+    pdf_canvas.setFont('HeiseiKakuGo-W5', font_size)
+    pdf_canvas.drawString(450, 770, '依頼日: ' + dt[0][2])
+
+    # 発注先
+    font_size = 14
+    pdf_canvas.setFont('HeiseiKakuGo-W5', font_size)
+    pdf_canvas.drawString(20, 770, dt[0][9] + '　' + dt[0][6] + '　' + dt[0][7])
+    pdf_canvas.line(20, 760, 240, 760) 
+
+    # 自社情報
+    font_size = 9
+    pdf_canvas.setFont('HeiseiKakuGo-W5', font_size)
+    pdf_canvas.drawString(380, 720, dt[0][23] + '　' + dt[0][31] + dt[0][32])
+    pdf_canvas.drawString(380, 700, '〒 ' + dt[0][24])
+    pdf_canvas.drawString(380, 680, dt[0][25] + dt[0][26] + dt[0][27] + dt[0][28])
+    pdf_canvas.drawString(380, 660, 'TEL: ' + dt[0][29] + '　FAX: ' + dt[0][30])
+    pdf_canvas.drawString(380, 640, 'E-mail: ' + dt[0][33])
+
+    # メッセージ
+    font_size = 9
+    pdf_canvas.setFont('HeiseiKakuGo-W5', font_size)
+    pdf_canvas.drawString(20, 620,'下 記 の 通 り 依 頼 い た し ま す 。')
+
+    # line
+    pdf_canvas.line(20, 610, 570, 610) 
+    pdf_canvas.line(290, 370, 290, 610) 
+
+    # 原糸メーカー
+    font_size = 9
+    pdf_canvas.setFont('HeiseiKakuGo-W5', font_size)
+    pdf_canvas.drawString(20, 590,'原糸メーカー')
+    pdf_canvas.line(20, 570, 570, 570) 
+
+    # 原糸出荷
+    font_size = 9
+    pdf_canvas.setFont('HeiseiKakuGo-W5', font_size)
+    pdf_canvas.drawString(20, 550,'原糸出荷')
+    pdf_canvas.line(20, 530, 570, 530) 
+
+    # 品番
+    font_size = 9
+    pdf_canvas.setFont('HeiseiKakuGo-W5', font_size)
+    pdf_canvas.drawString(20, 510,'品番')
+    #pdf_canvas.drawString(80, 500, dt[0][5])
+    pdf_canvas.line(20, 490, 570, 490) 
+
+    # 品名
+    font_size = 9
+    pdf_canvas.setFont('HeiseiKakuGo-W5', font_size)
+    pdf_canvas.drawString(20, 470,'品名')
+    #pdf_canvas.drawString(80, 460, dt[0][3])
+    pdf_canvas.line(20, 450, 570, 450) 
+
+    # 番手
+    font_size = 9
+    pdf_canvas.setFont('HeiseiKakuGo-W5', font_size)
+    pdf_canvas.drawString(20, 430,'番手')
+    #pdf_canvas.drawString(80, 420, dt[0][4])
+    pdf_canvas.line(20, 410, 570, 410) 
+
+    # 混率
+    font_size = 9
+    pdf_canvas.setFont('HeiseiKakuGo-W5', font_size)
+    pdf_canvas.drawString(20, 390,'混率')
+    #pdf_canvas.drawString(80, 380, dt[0][42])
+    pdf_canvas.line(20, 370, 570, 370) 
+
+    # 希望納期
+    font_size = 9
+    pdf_canvas.setFont('HeiseiKakuGo-W5', font_size)
+    pdf_canvas.drawString(300, 590,'希望納期')
+    #pdf_canvas.drawString(360, 580, dt[0][8])
+
+    # 回答納期
+    font_size = 9
+    pdf_canvas.setFont('HeiseiKakuGo-W5', font_size)
+    pdf_canvas.drawString(300, 550,'回答納期')
+    #pdf_canvas.drawString(360, 540, dt[0][2])
+
+    # 不明
+    font_size = 9
+    pdf_canvas.setFont('HeiseiKakuGo-W5', font_size)
+    pdf_canvas.drawString(300, 510,'')
+
+    # 仕入単価
+    font_size = 9
+    pdf_canvas.setFont('HeiseiKakuGo-W5', font_size)
+    pdf_canvas.drawString(300, 470,'仕入単価')
+    #pdf_canvas.drawString(360, 460, dt[0][20])
+
+    # 出荷先名
+    font_size = 9
+    pdf_canvas.setFont('HeiseiKakuGo-W5', font_size)
+    pdf_canvas.drawString(300, 430,'出荷先名')
+    #pdf_canvas.drawString(360, 420, dt[0][34])
+
+    # 項番、色番、カラー、仕立、数量、摘要
+    style = ParagraphStyle(name='Normal', fontName='HeiseiKakuGo-W5', fontSize=9, alignment=TA_CENTER)
+    itemNo0 = Paragraph('項番',style)
+    itemNo1 = Paragraph('色番',style)
+    itemNo2 = Paragraph('カラー',style)
+    itemNo3 = Paragraph('仕立',style)
+    itemNo4 = Paragraph('数量',style)
+    itemNo5 = Paragraph('摘要',style)
+
+    data = [
+        [itemNo0, itemNo1, itemNo2, itemNo3, itemNo4, itemNo5],
+    ]
+
+    table = Table(data, colWidths=(20*mm, 30*mm, 30*mm, 15*mm, 30*mm, 70*mm), rowHeights=7.5*mm)
+    table.setStyle(TableStyle([
+            ('FONT', (0, 0), (-1, -1), 'HeiseiKakuGo-W5', 9),
+            ('BOX', (0, 0), (-1, -1), 1, colors.black),
+            ('INNERGRID', (0, 0), (-1, -1), 1, colors.black),
+            ('VALIGN', (0, 0), (-1, -1), 'MIDDLE'),
+        ]))
+    table.wrapOn(pdf_canvas, 7*mm, 10*mm)
+    table.drawOn(pdf_canvas, 7*mm, 118.0*mm)
+
+    data =[]
+    l=len(dt)
+    styleLeft = ParagraphStyle(name='Normal', fontName='HeiseiKakuGo-W5', fontSize=9, alignment=TA_LEFT)
+    styleRight = ParagraphStyle(name='Normal', fontName='HeiseiKakuGo-W5', fontSize=9, alignment=TA_RIGHT)
+
+    for i in range(15):
+        if i<l: 
+            row = dt[i]
+            # 指定した列の左寄せ
+            DetailColorNumber = Paragraph(row[16],styleLeft)
+            DetailColor = Paragraph(row[17],styleLeft)
+            DetailTailoring = Paragraph(row[18],styleLeft)
+            DetailSummary = Paragraph(row[21],styleLeft)
+            # 指定した列の右寄せ
+            DetailItemNumber = Paragraph(row[15],styleRight)
+            Volume = Paragraph(row[19],styleRight)
+
+            data += [
+                    [DetailItemNumber, DetailColorNumber, DetailColor, DetailTailoring, Volume, DetailSummary],
+            ]
+        else:
+            data += [
+                    ['','','','','',''],
+            ]
+
+        table = Table(data, colWidths=(20*mm, 30*mm, 30*mm, 15*mm, 30*mm, 70*mm), rowHeights=7.5*mm)
+        table.setStyle(TableStyle([
+                ('FONT', (0, 0), (-1, -1), 'HeiseiKakuGo-W5', 9),
+                ('BOX', (0, 0), (-1, -1), 1, colors.black),
+                ('INNERGRID', (0, 0), (-1, -1), 1, colors.black),
+                ('VALIGN', (0, 0), (-1, -1), 'MIDDLE'),
+            ]))
+
+    table.wrapOn(pdf_canvas, 7*mm, 10*mm)
+    table.drawOn(pdf_canvas, 7*mm, 5.5*mm)
  
     pdf_canvas.showPage()
      
